84b0540fd28e7a62a0a2b5a4889b3762f0394c3f
Tiago Seabra <tlgs@users.noreply.github.com>
2024-12-06 09:55:30 +0000

    Refactor day 05
    Split between correct and incorrect updates during parsing to avoid
    repeated computation in both parts.

--- 05.py
+++ 05.py
@@ -6,63 +6,43 @@ def parse_input(puzzle_input):
 
     rules = []
     for line in fst.splitlines():
-        rule = tuple([int(v) for v in line.split("|")])
+        rule = [int(v) for v in line.split("|")]
         rules.append(rule)
 
-    updates = []
+    correct, incorrect = [], []
     for line in snd.splitlines():
-        update = tuple([int(v) for v in line.split(",")])
-        updates.append(update)
-
-    return rules, updates
-
-
-def part_one(rules, updates):
-    total = 0
-    for update in updates:
+        update = [int(v) for v in line.split(",")]
         for left, right in rules:
-            try:
-                if update.index(left) > update.index(right):
-                    break
-            except ValueError:
-                pass
-
+            if {left, right} <= set(update) and (
+                update.index(left) > update.index(right)
+            ):
+                incorrect.append(update)
+                break
         else:
-            total += update[len(update) // 2]
+            correct.append(update)
 
-    return total
+    return rules, correct, incorrect
 
 
-def part_two(rules, updates):
-    to_fix = []
-    for update in updates:
-        flag, relevant = False, []
-        for left, right in rules:
-            try:
-                if update.index(left) > update.index(right):
-                    flag = True
+def part_one(rules, correct, incorrect):
+    return sum(update[len(update) // 2] for update in correct)
 
-                relevant.append((left, right))
-            except ValueError:
-                pass
 
-        if flag:
-            to_fix.append((list(update), relevant))
-
+def part_two(rules, correct, incorrect):
     total = 0
-    for update, update_rules in to_fix:
+    for update in incorrect:
+        relevant_rules = list(filter(lambda t: set(t) <= set(update), rules))
         while True:
             done = True
-            for left, right in update_rules:
+            for left, right in relevant_rules:
                 a, b = update.index(left), update.index(right)
                 if a > b:
-                    update[a], update[b] = right, left
+                    update[a], update[b] = update[b], update[a]
                     done = False
 
             if done:
+                total += update[len(update) // 2]
                 break
-
-        total += update[len(update) // 2]
 
     return total
 
